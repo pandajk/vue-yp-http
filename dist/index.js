96aa9f6ba7f183d8e71d02e4e5657e370c6627f8
PandaJ <zjarek@me.com>
2018-08-29 11:38:40 +0800

    feat: 优化项目文件

---
+++ dist/index.js
@@ -0,0 +1,224 @@
+/*
+ * @Author: panda
+ * @Date:   2018-07-10 15:33:18
+ * @Last Modified by:   PandaJ
+ * @Last Modified time: 2018-08-28 16:30:18
+ */
+
+let is_debug = false;
+if (/ypdebug/.test(navigator.userAgent.toLowerCase())) {
+  is_debug = true;
+}
+
+import axios from 'axios';
+
+function install(Vue, options) {
+  const _opt = Object.assign({
+    // baseURL: '/',
+    // path: '',
+    // timeout: 5000,
+    // headers: {}
+    method: '$http'
+  }, options);
+
+  _opt.baseURL && (axios.default.baseURL = _opt.baseURL);
+
+  Vue.prototype[_opt.method] = {};
+
+  const post = function(method, params, options) {
+    return new Promise((resolve, reject) => {
+      axios.post(options && options.path || _opt.path, {
+        method,
+        biz_content: params
+      }).then(resp => {
+        is_debug && console.group();
+        is_debug && console.log(`%c POST %c ${method} `, 'background: #222; color: #bada55', 'background: green; color: white');
+        is_debug && console.log('%c PARAMS ', 'background: #222; color: #fff', params);
+        is_debug && console.log('%c RESPONSE ', 'background: #222; color: yellow', resp);
+        is_debug && console.groupEnd();
+        resolve(resp);
+      }).catch((err, resp) => {
+        is_debug && console.group();
+        is_debug && console.error(`%c POST %c ${method} `, 'background: #222; color: #bada55', 'background: green; color: white');
+        is_debug && console.error('%c PARAMS ', 'background: #222; color: #fff', params);
+        is_debug && console.error('%c RESPONSE ', 'background: #222; color: yellow', resp);
+        is_debug && console.groupEnd();
+        reject(err, resp)
+      })
+    })
+  };
+
+
+  const get = function(method, params, options) {
+    return new Promise((resolve, reject) => {
+
+      axios.get(options && options.path || _opt.path, {
+        params: {
+          method,
+          biz_content: params
+        }
+      }).then(resp => {
+        is_debug && console.group();
+        is_debug && console.log(`%c GET %c ${method} `, 'background: #222; color: #bada55', 'background: green; color: white');
+        is_debug && console.log('%c PARAMS ', 'background: #222; color: #fff', params);
+        is_debug && console.log('%c RESPONSE ', 'background: #222; color: yellow', resp);
+        is_debug && console.groupEnd();
+        resolve(resp);
+      }).catch((err, resp) => {
+        iis_debug && console.group();
+        is_debug && console.error(`%c GET %c ${method} `, 'background: #222; color: #bada55', 'background: green; color: white');
+        is_debug && console.error('%c PARAMS ', 'background: #222; color: #fff', params);
+        is_debug && console.error('%c RESPONSE ', 'background: #222; color: yellow', resp);
+        is_debug && console.groupEnd();
+        reject(err, resp)
+      })
+    });
+  };
+
+  const upload = function(params, options) {
+    return new Promise((resolve, reject) => {
+      is_debug && console.log(`%c UPLOAD `, 'background: #222; color: #bada55', params);
+      try {
+        let _options;
+        let formData;
+        if (!options || !options.type || options.type == 'form-data') {
+          _options = {
+            header: {
+              'Content-Type': 'multipart/form-data'
+            }
+          };
+          formData = new FormData();
+          Object.keys(params).map(key => {
+            if (Object.prototype.toString.call(params[key]) == '[object Array]') {
+              formData.append(key, params[key][0], params[key][1] || '');
+            } else {
+              formData.append(key, params[key]);
+            }
+          })
+        }
+
+        _options = Object.assign(_options, options.options);
+
+
+        resolve(axios.post(options && options.path || _opt.path, formData, _options));
+      } catch (err) {
+        is_debug && console.error(`%c UPLOAD `, 'background: #222; color: #bada55', resp);
+        reject(err)
+      }
+    })
+  }
+
+  const download = function(method, params, options) {
+    return new Promise((resolve, reject) => {
+      is_debug && console.log(`%c DOWNLOAD %c ${method} `, 'background: #222; color: #bada55', 'background: green; color: white');
+      is_debug && console.log('%c PARAMS ', 'background: #222; color: #fff', params);
+
+      let _options = Object.assign({
+        responseType: 'blob'
+      }, options && options.options);
+
+      axios.post(options && options.path || _opt.path, {
+        method,
+        biz_content: params
+      }, _options).then((resp) => {
+        try {
+          parseResponseToJSON(resp.data)
+            .then(response => {
+              is_debug && console.log('%c DOWNLOAD RESPONSE ', 'background: #222; color: yellow', response);
+
+
+              resolve(Object.assign(resp, {
+                data: response
+              }));
+            }).catch(err => {
+              const blob = new Blob([resp.data], {
+                type: resp.data.type
+              })
+
+              var downloadElement = document.createElement('a');　　
+              var href = window.URL.createObjectURL(blob); //创建下载的链接
+              　　
+              downloadElement.href = href;
+              let tmp = decodeURIComponent(resp.headers['content-disposition']);
+              tmp = tmp.match(/filename\*=UTF-8''\W+\.\w+/);
+
+              let filename;
+              if (options && options.filename) {
+                filename = options.filename;
+              } else {
+                filename = +new Date();
+                if (tmp) {
+                  filename = tmp[0].replace("filename*=UTF-8''", '');
+                }　　
+              }
+
+              downloadElement.download = filename; //下载后文件名
+              　　
+              document.body.appendChild(downloadElement);　　
+              downloadElement.click(); //点击下载
+              　　
+              document.body.removeChild(downloadElement); //下载完成移除元素
+              　　
+              window.URL.revokeObjectURL(href); //释放掉blob对象 
+              resolve(resp);
+            })
+
+        } catch (err) {
+          is_debug && console.error('%c DOWNLOAD ', 'background: #222; color: yellow', err);
+          reject(err)
+        }
+      }).catch(err => {
+        is_debug && console.error('%c DOWNLOAD ', 'background: #222; color: yellow', err);
+
+        reject(err);
+      })
+    })
+  }
+
+  Vue.prototype[_opt.method].get = get;
+  Vue.prototype[_opt.method].post = post;
+  Vue.prototype[_opt.method].upload = upload;
+  Vue.prototype[_opt.method].download = download;
+
+  Vue.prototype.$axios = axios;
+
+  Vue.yphttp = {
+    get: get,
+    post: post,
+    upload: upload,
+    download: download
+  };
+}
+
+// for download method
+// parse blob type response to json
+function parseResponseToJSON(data) {
+  return new Promise((resolve, reject) => {
+    try {
+      let fr = new FileReader();
+      fr.onload = function() {
+        try {
+
+          const tmpResp = JSON.parse(this.result);
+          if (tmpResp.error_code != 0) {
+            resolve(tmpResp);
+          }
+        } catch (err) {
+          reject(err)
+        }
+      }
+      fr.readAsText(data);
+    } catch (err) {
+      reject(err)
+    }
+  })
+}
+
+export default install;
+
+if (typeof window !== 'undefined' && window.Vue) {
+  window.Vue.use(install);
+  if (install.installed) {
+    install.installed = false;
+  }
+}
